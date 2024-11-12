a42cd20107bed75efdf64ba336de809a3371c844
nzlov <wfhtqp@gmail.com>
2024-11-12 11:07:43 +0800

    clean

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,14 @@
-return {
+local _float_console = function()
+  require("dapui").float_element("console", {
+    enter = true,
+    title = "console",
+    position = "center",
+    height = math.floor(vim.o.lines * 0.9),
+    width = math.floor(vim.o.columns * 0.8),
+  })
+end
 
+return {
   { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
@@ -64,6 +73,45 @@ return {
     lazy = true,
     run = function()
       vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    "javiorfo/nvim-wildcat",
+    lazy = true,
+    ft = "java",
+    cmd = { "WildcatRun", "WildcatUp", "WildcatInfo" },
+    dependencies = { "javiorfo/nvim-popcorn" },
+    opts = {
+      -- Not necessary. Only if you want to change the setup
+      -- The following are the default values
+
+      console_size = 15,
+      tomcat = {
+        home = "/home/nzlov/workspaces/apache-tomcat-9.0.97",
+        app_base = "webapps",
+        default = true,
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    keys = {
+      {
+        "<leader>dd",
+        _float_console,
+        desc = "Dap UI console",
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-o>"] = cmp.mapping.complete(),
+        ["<CR>"] = LazyVim.cmp.confirm({ select = true }),
+      })
     end,
   },
 }
