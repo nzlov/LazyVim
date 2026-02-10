00211f9785dc3cd6b5da3700f4e60f53c7e3a7f7
wfhtqp@gmail.com <wfhtqp@gmail.com>
2026-02-10 10:59:46 +0800

    feat: snacks use float terminal

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -93,4 +93,14 @@ return {
   --     vim.g.tabby_inline_completion_trigger = "auto"
   --   end,
   -- },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "float",
+        },
+      },
+    },
+  },
 }
